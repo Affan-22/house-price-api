a9fbb9ac3462c2f6df9068ea67d4ecbeafc7587b
Affan-22 <23btrdc066@jainuniversity.ac.in>
2025-11-18 12:24:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,13 +48,14 @@ if st.button("Predict Price"):
     x = torch.tensor([[sqft, bhk]], dtype=torch.float32)
     prediction = model(x).item()
 
-    st.success(f"Predicted Price: ₹ {prediction^2:,.3f}")
+    st.success(f"Predicted Price: ₹ {prediction*10:,.3f}")
 
     st.caption("⚠️ Note: This is an untrained model — predictions are random unless trained.")
 
 # Footer
 st.write("---")
 st.write("Developed with ❤️ using Streamlit + PyTorch")
+
 
 
 
